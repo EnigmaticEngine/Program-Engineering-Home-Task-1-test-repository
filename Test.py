0096ad0a4fb6f8d4270237db28b9f484f42dd9b2
EnigmaticEngine <147271333+EnigmaticEngine@users.noreply.github.com>
2025-03-16 23:59:31 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,28 @@
 
-wNumber = int(input("Please, state the number of the current worldline: "))
-print("Hello, Worldline #", wNumber)
+# wNumber = int(input("Please, state the number of the current worldline: "))
+# print("Hello, Worldline #", wNumber)
+
+import unittest
+import main
+
+class TestStringMethods(unittest.TestCase):
+    
+    def test_Dud(self):
+
+        self.assertEqual(main.Dud(), "A!")
+
+    def test_inputControl(self):
+        
+        self.assertEqual(main.inputControl(), 1)
+
+    def test_characterChoiceConfirmation(self):
+
+        self.assertTrue(main.characterChoiceConfirmation())
+
+    def test_harderInputControl(self):
+
+        self.assertIsNotNone(main.harderInputContol())
+
+if __name__ == '__main__':
+
+    unittest.main()
