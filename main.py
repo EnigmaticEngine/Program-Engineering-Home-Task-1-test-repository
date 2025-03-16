0f433d71fd1a3be7f1a36a000e7b7dbedaa7949a
EnigmaticEngine <147271333+EnigmaticEngine@users.noreply.github.com>
2025-03-16 23:58:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+def Dud():
+
+    return 'A!'
+
+def inputControl():
+    
+    alfa = int(input("Введите число: "))
+
+    return alfa
+
+def characterChoiceConfirmation():
+
+    gamma = input('Вы уверены в выборе данного имени? (y/n): ').lower().strip() == 'y'
+
+    return gamma
+
+def harderInputContol():
+
+    beta = str(input("Введите имя персонажа: "))
+
+    return beta
+
+    
