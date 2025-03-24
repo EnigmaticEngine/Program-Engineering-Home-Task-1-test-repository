931f82a2e127ffcc442007fe4ee6f3a3ed46694d
EnigmaticEngine <147271333+EnigmaticEngine@users.noreply.github.com>
2025-03-24 11:52:52 +0300

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -20,6 +20,6 @@ class TestStringMethods(unittest.TestCase):
 
         self.assertIsNotNone(main.harder_input_control())
     @patch('builtins.input', return_value='1')
+    
 if __name__ == '__main__':
-
     unittest.main()
