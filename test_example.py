101e196169532873e4cd54d01035e1781332b807
EnigmaticEngine <147271333+EnigmaticEngine@users.noreply.github.com>
2025-03-24 12:29:15 +0300

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -16,13 +16,9 @@ class TestStringMethods(unittest.TestCase):
 
         self.assertTrue(main.characterChoiceConfirmation())
 
-    def test_harderInputControl(self):
-
-        self.assertIsNotNone(main.harder_input_control())
-    @patch('builtins.input', return_value='5')
-    
     def test_harderInputControl(self, mock_input):
         self.assertIsNotNone(main.harder_input_control())
+    @patch('builtins.input', return_value='5')
     
 if __name__ == '__main__' :
     unittest.main()
